5ef2ec3581588c0161909416147d6bc76ac1db25
targax <brunotarga08@gmail.com>
2025-08-08 20:00:01 -0300

    update code - post veiculo

--- Dominio/Entidades/Veiculo.cs
+++ Dominio/Entidades/Veiculo.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MinimalApi.Dominio.Entidades
 {
-    public class Administrador
+    public class Veiculo
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
@@ -11,16 +11,15 @@ namespace MinimalApi.Dominio.Entidades
         public int Id { get; set; } = default!;
 
         [Required]
-        [StringLength(255)]
-        public string Email { get; set; } = default!;
+        [StringLength(150)]
+        public string Nome { get; set; } = default!;
         
         [Required]
-        [StringLength(50)]
-        public string Senha { get; set; } = default!;
+        [StringLength(100)]
+        public string Marca { get; set; } = default!;
 
         [Required]
-        [StringLength(10)]
-        public string Perfil { get; set; } = default!;
+        public int Ano { get; set; } = default!;
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,10 +5,17 @@ using MinimalApi.Dominio.Interfaces;
 using MinimalApi.Dominio.Entidades;
 using MinimalApi.Dominio.Servicos;
 using Microsoft.AspNetCore.Mvc;
+using MinimalApi.Dominio.ModelViews;
+
+#region Builder
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
+builder.Services.AddScoped<IVeiculoServico, VeiculoServico>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<DbContexto>(options =>{
     options.UseMySql(
@@ -18,16 +25,41 @@ builder.Services.AddDbContext<DbContexto>(options =>{
 });
 
 var app = builder.Build();
+#endregion
 
-app.MapGet("/", () => "Hello World!");
+#region Home
+app.MapGet("/", () => Results.Json(new Home()));
+#endregion
 
-app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) =>
+#region Administradores
+app.MapPost("/administradores/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) =>
 {
-    if(administradorServico.Login(loginDTO) != null)
+    if (administradorServico.Login(loginDTO) != null)
     {
         return Results.Ok("Login successful!");
     }
     return Results.Unauthorized();
 });
+#endregion
+
+#region Veiculos
+app.MapPost("/veiculos", ([FromBody] VeiculoDTO veiculoDTO, IVeiculoServico veiculoServico) =>
+{
+    var veiculo = new Veiculo{
+        Nome = veiculoDTO.Nome,
+        Marca = veiculoDTO.Marca,
+        Ano = veiculoDTO.Ano
+
+    };
+    veiculoServico.Incluir(veiculo);
+
+    return Results.Created($"/veiculo/{veiculo.Id}", veiculo);
+});
+#endregion
+
+#region App
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
+#endregion
