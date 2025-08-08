7a3ec713b428890dbe1a015c5d279241497547c3
targax <brunotarga08@gmail.com>
2025-08-08 00:01:14 -0300

    update

--- Program.cs
+++ Program.cs
@@ -1,9 +1,21 @@
+using MinimalApi.Infraestrutura.Db;
+using MinimalApi.DTOs;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(options =>{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/login", (MinimalApi.DTOs.LoginDTO loginDto) =>
+app.MapPost("/login", (LoginDTO loginDto) =>
 {
     if (loginDto.Email == "user@example.com" && loginDto.Password == "password")
     {
