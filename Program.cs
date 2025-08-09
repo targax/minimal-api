01900c9de7bcd336155a639e4b2a47175a4270cd
targax <brunotarga08@gmail.com>
2025-08-08 23:53:51 -0300

    get veiculo

--- Program.cs
+++ Program.cs
@@ -56,11 +56,20 @@ app.MapPost("/veiculos", ([FromBody] VeiculoDTO veiculoDTO, IVeiculoServico veic
     return Results.Created($"/veiculo/{veiculo.Id}", veiculo);
 }).WithTags("Veiculos");
 
-app.MapGet("/veiculos", ([FromQuery]int? pagina, IVeiculoServico veiculoServico) =>
+app.MapGet("/veiculos", ([FromQuery] int? pagina, IVeiculoServico veiculoServico) =>
 {
     var veiculos = veiculoServico.Todos(pagina);
 
     return Results.Ok(veiculos);
+}).WithTags("Veiculos");
+
+app.MapGet("/veiculos/{id}", ([FromRoute] int id, IVeiculoServico veiculoServico) =>
+{
+    var veiculo = veiculoServico.BuscaPorid(id);
+
+    if(veiculo == null) return Results.NotFound();
+
+    return Results.Ok(veiculo);
 }).WithTags("Veiculos");
 #endregion
 
