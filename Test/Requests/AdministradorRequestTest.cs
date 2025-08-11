3c0083d4e703453ea59dbf9a5d21140308be0c5d
targax <brunotarga08@gmail.com>
2025-08-11 16:16:46 -0300

    create startup

---
+++ Test/Requests/AdministradorRequestTest.cs
@@ -0,0 +1,16 @@
+namespace Test.Requests;
+
+[TestClass]
+public class AdministradorRequestTest
+{
+    [TestMethod]
+    public void TestarGetSetPropriedades()
+    {
+        // Arrange
+
+        // Act
+        
+        // Assert
+        
+    }
+}
